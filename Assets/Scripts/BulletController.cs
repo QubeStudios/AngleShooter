8352c5583a09f393793befcbc2121627748a5795
Christianrg <christian.rojasg@pucp.edu.pe>
2018-01-17 13:47:29 -0500

    Project Base
    Project Base AngleShooter

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+
+	public static int selected_angle = 0;
+	public int angle = 0;
+	public int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown(){
+		selected_angle = angle;
+		Debug.Log ("Selected :" + angle);
+	}
+}
